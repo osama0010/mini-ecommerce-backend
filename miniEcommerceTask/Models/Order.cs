a3d1c681283e99ad316b404b28e42758b60f92d3
osama0010 <osama.eidev@gmail.com>
2026-04-07 14:48:33 +0200

    dbCOntext and Models

---
+++ miniEcommerceTask/Models/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace miniEcommerceTask.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public decimal Total { get; set; }
+        public decimal Discount { get; set; }
+        public List<OrderItem> Items { get; set; }
+    }
+}
---
+++ miniEcommerceTask/Models/OrderItem.cs
@@ -0,0 +1,13 @@
+﻿namespace miniEcommerceTask.Models
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+
+
+        public Product Product { get; set; }
+    }
+}
