5f6062fecf0fbb6cd48452c5079bd444bd1bd85b
osama0010 <osama.eidev@gmail.com>
2026-04-07 22:29:36 +0200

    MVC Added

---
+++ miniEcommerceTask.MVC/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using miniEcommerceTask.MVC.Models;
+
+
+namespace miniEcommerceTask.MVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly HttpClient _client;
+
+        public ProductsController(IHttpClientFactory factory)
+        {
+            _client = factory.CreateClient();
+            _client.BaseAddress = new Uri("https://localhost:7118/");
+        }
+        public async Task<IActionResult> Index()
+        {
+            var products = await _client.GetFromJsonAsync<List<Product>>("api/products");
+            return View(products);
+        }
+    }
+}
---
+++ miniEcommerceTask.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace miniEcommerceTask.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
